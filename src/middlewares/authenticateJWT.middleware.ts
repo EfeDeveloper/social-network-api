a058709d6c80ffe322fbbe577e92bee214e56823
EdferVIIIA <edwinvilla_adsi@outlook.com>
2025-07-01 18:51:24 -0500

    ✨[middlewares]Add JWT authentication middleware - Implemented JWT authentication to secure routes. - Validates the presence and format of the authorization token. - Handles errors for invalid or expired tokens.

---
+++ src/middlewares/authenticateJWT.middleware.ts
@@ -0,0 +1,25 @@
+import { Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { handleControllerError } from '../utils/errorResponse.js';
+import { AuthRequest, JwtPayload } from '../types/jwt.js';
+
+const JWT_SECRET = process.env.JWT_SECRET as string;
+
+export const authenticateJWT = (req: AuthRequest, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    res.status(401).json({ error: 'Access Denied, no token provided' });
+    return;
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+    req.user = decoded;
+    next();
+  } catch (error) {
+    handleControllerError(res, error, 'Invalid or expired token');
+  }
+};
