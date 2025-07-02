eb51262e874f695e23f8098ce9c46bb7bb173ba7
EdferVIIIA <edwinvilla_adsi@outlook.com>
2025-07-02 11:06:58 -0500

    ✨[api]Add Swagger documentation route and update console logs - Introduced a new route for serving Swagger documentation at '/docs'. - Updated console logs to include a message about the Swagger documentation availability. - Improved code readability by organizing eslint directives.

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import app from './app.js';
 const PORT = process.env.PORT || 3005;
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
+  /* eslint-disable no-console */
   console.log(`🚀  -- Server is running on: http://localhost:${PORT}`);
+  console.log(`🚀  -- Swagger documentation available at http://localhost:${PORT}/api/docs`);
+  /* eslint-enable no-console */
 });
