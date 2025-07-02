12ce3f973cf7921db3fbad2098779d793c5d56bf
EdferVIIIA <edwinvilla_adsi@outlook.com>
2025-07-01 19:23:35 -0500

    ✨[controllers]Add createPostController for post creation - Implemented createPostController to handle post creation requests. - Validates user authentication and message presence before creating a post. - Returns appropriate HTTP status codes and error messages for different scenarios.

---
+++ src/controllers/post.controller.ts
@@ -0,0 +1,26 @@
+import { Response } from 'express';
+import { AuthRequest } from '../types/jwt.js';
+import { createPost } from '../services/post.service.js';
+import { handleControllerError } from '../utils/errorResponse.js';
+
+export const createPostController = async (req: AuthRequest, res: Response) => {
+  try {
+    const userId = req.user?.userId;
+    const { message } = req.body;
+
+    if (!userId) {
+      res.status(401).json({ error: 'User not authenticated.' });
+      return;
+    }
+    if (!message) {
+      res.status(400).json({ error: 'The message is required.' });
+      return;
+    }
+
+    const post = await createPost(userId, message);
+
+    res.status(201).json(post);
+  } catch (error) {
+    handleControllerError(res, error, 'Error when creating the publication.');
+  }
+};
